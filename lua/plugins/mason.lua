cdb2f1b0a8b653f7f2597f58f69f8925b3377002
jonas_wsl <jonasjuulhansen@hotmail.com>
2025-08-08 19:52:13 +0200

    xx

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -42,6 +42,7 @@ return {
 					require("lspconfig")[server_name].setup({
 						vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { noremap = true, silent = true, desc='Goto definition' });
 						vim.keymap.set('n', 'gr', vim.lsp.buf.references, { noremap = true, silent = true, desc='Goto references' });
+						vim.keymap.set('n', 'K', vim.lsp.buf.hover , { noremap = true, silent = true, desc='Hover' });
 					})
 				end,
 			})
