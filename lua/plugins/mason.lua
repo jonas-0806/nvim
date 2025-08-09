ffda4a408a2d4aa2cfa1f5fd8f8025efca9eb70a
jonas_wsl <jonasjuulhansen@hotmail.com>
2025-08-09 12:42:01 +0200

    zz

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,72 +1,51 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = { "saghen/blink.cmp" },
-    opts = {
-      servers = {
-        lua_ls = {},
-        zls = {
-          settings = {
-            filetypes = { "zig", "zon" },
-            zls = {
-              enable_snippets = true,
-            },
-          },
-        },
-      },
-    },
-    config = function(_, opts)
-      local lspconfig = require("lspconfig")
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = { 'saghen/blink.cmp' },
+		-- example using `opts` for defining servers
+		opts = {
+			servers = {
+				lua_ls = {},
+				zls = {
+					settings = {
+						filetypes = {"zig","zon"},
+						zls = {
+							enable_snippets = true,
+						},
+					},
+				},
+			},
+		},
+		config = function(_, opts)
+			local lspconfig = require('lspconfig')
+			for server, config in pairs(opts.servers) do
+				-- passing config.capabilities to blink.cmp merges with the capabilities in your
+				-- `opts[server].capabilities, if you've defined it
+				config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+				lspconfig[server].setup(config)
+			end
+		end
 
-      local on_attach = function(_, bufnr)
-        local opts = { noremap = true, silent = true, buffer = bufnr }
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-      end
-
-    end,
-  },
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = { "mason.nvim" },
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "zls" },
-      })
-
-      local lspconfig = require("lspconfig")
-      local servers = {
-        lua_ls = {},
-        zls = {
-          settings = {
-            filetypes = { "zig", "zon" },
-            zls = { enable_snippets = true },
-          },
-        },
-      }
-
-      local on_attach = function(_, bufnr)
-        local opts = { noremap = true, silent = true, buffer = bufnr }
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-      end
-
-      local cmp_capabilities = require("blink.cmp").get_lsp_capabilities()
-
-      for _, server_name in ipairs(require("mason-lspconfig").get_installed_servers()) do
-        local server_opts = servers[server_name] or {}
-        server_opts.on_attach = on_attach
-        server_opts.capabilities = vim.tbl_extend("force", cmp_capabilities, server_opts.capabilities or {})
-        lspconfig[server_name].setup(server_opts)
-      end
-    end,
-  },
+	},
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies =  { "mason.nvim" },
+		config = function()
+			require("mason-lspconfig").setup({
+				function(server_name)
+					require("lspconfig")[server_name].setup({
+						vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { noremap = true, silent = true, desc='Goto definition' });
+						vim.keymap.set('n', 'gr', vim.lsp.buf.references, { noremap = true, silent = true, desc='Goto references' });
+						vim.keymap.set('n', 'K', vim.lsp.buf.hover , { noremap = true, silent = true, desc='Hover' });
+					})
+				end,
+			})
+		end,
+	}
 }
--- lua/plugins/sitruuna.lua
+++
@@ -1,3 +0,0 @@
-return {
-	"eemed/sitruuna.vim",
-}
