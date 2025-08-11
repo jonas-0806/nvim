5bd8b39137992ca3fb1a9ff30a1eef63eb780599
jonas_wsl <jonasjuulhansen@hotmail.com>
2025-08-11 22:51:49 +0200

    telescope keybinds

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,7 +12,8 @@ return {
 			}
 		}
 
-		vim.keymap.set('n', '<leader>fg', builtin.lsp_document_symbols, { desc = 'Telescope find symbols in current buffer' })
+		vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, { desc = 'Telescope find symbols in current buffer' })
+		vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 		vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'telescope find files' })
 		vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 		vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
